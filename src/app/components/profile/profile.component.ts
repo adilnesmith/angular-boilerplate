122e75e9fc53b6009b962cf762564bc6be2e4c48
Adil Peter <adilnesmith@ymail.com>
2023-05-26 10:39:50 +0500

    feat: edit user functionality

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -9,11 +9,14 @@ import { User } from '../../lib/types/user';
 })
 export class ProfileComponent {
   users!: User[];
+  selectedUser: any;
+  editMode = false;
   constructor(private apiService: ApiService,) { }
   ngOnInit() {
     this.fetchUserList();
   }
   addUser(userForm: NgForm) {
+    this.editMode = false;
     if (userForm.valid) {
       const payload = {
         fullName: userForm.value.fullName,
@@ -33,14 +36,38 @@ export class ProfileComponent {
       this.users = _users;
     });
   }
-  editUser(id: User["_id"]) {
-
+  populateFields(user: User, userForm: NgForm) {
+    this.editMode = true;
+    this.selectedUser = user;
+    userForm.setValue({
+      fullName: user.fullName,
+      email: user.email,
+      age: user.age,
+      password: user.password
+    });
+    userForm.controls['password']?.disable();
+    this.editUser(userForm);
+  }
+  editUser(userForm: NgForm) {
+    if (userForm.valid) {
+      const payload = {
+        fullName: userForm.value.fullName,
+        email: userForm.value.email,
+        age: userForm.value.age,
+        password: userForm.value.password
+      };
+      if (userForm.touched) {
+        this.apiService.put("users/" + this.selectedUser?._id, payload).subscribe(() => {
+          this.fetchUserList();
+        });
+      }
+    }
   }
   deleteUser(id: User["_id"]) {
     let confirmAction = confirm("Are you sure to delete this user?");
     if (confirmAction) {
       this.apiService.delete("users/" + id).subscribe(() => {
-        this.users = this.users.filter(user => user._id !== id);
+        this.fetchUserList();
         alert("user removed");
       });
     }
