da81c2bb9d52d41e6904c909dd915d7c4591b217
Adil Peter <adilnesmith@ymail.com>
2023-05-30 11:14:18 +0500

    feat: user login endpoint added

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import { ApiService } from 'src/app/users.api.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  login() {
-
+  constructor(private apiService: ApiService,) { }
+  login(loginForm: NgForm) {
+    if (loginForm.valid) {
+      const payload = {
+        email: loginForm.value.email,
+        password: loginForm.value.password
+      };
+      this.apiService.post("auth/login", payload).subscribe(() => {
+        loginForm.reset();
+      });
+    }
   }
 }
